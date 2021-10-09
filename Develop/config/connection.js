aaa115bf30ced82fdd96de97a23bfc93cc957326
ibraim martinez <ibraimm5@gmail.com>
2021-10-09 09:37:43 -0400

    adding connection.js

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,4 +1,6 @@
 const { connect, connection } = require('mongoose');
 
 const connectionString = 
-    process.env.MONGODB_URI || 'mongodb://localhost/'
+    process.env.MONGODB_URI || 'mongodb://localhost/workout' 
+
+module.exports = connection;
