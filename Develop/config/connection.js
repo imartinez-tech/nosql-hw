bea7b527dd59054d47a0bae1aca7c5e8bf237b56
ibraim martinez <ibraimm5@gmail.com>
2021-10-09 15:25:28 -0400

    connecting heroku and mongodb atlas

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,6 +1,14 @@
 const { connect, connection } = require('mongoose');
 
+
 const connectionString = 
-    process.env.MONGODB_URI || 'mongodb://localhost/workout' 
+    process.env.MONGODB_URI || 'mongodb://localhost/workoutdb' 
+
+connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
 
 module.exports = connection;
