b154bf2f58bb4d08103499dd109ae2400949e613
ibraim martinez <ibraimm5@gmail.com>
2021-10-14 23:06:39 -0400

    changing the connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,12 @@
 const { connect, connection } = require('mongoose');
 
 
-const connectionString = 
-    process.env.MONGODB_URI || 'mongodb://localhost/workoutDB' 
+// const connectionString = 
+//     process.env.MONGODB_URI || 'mongodb://localhost/workoutDB' 
 
-connect(connectionString, {
+
+connect(process.env.MONGODB_URI || 'mongodb://localhost/workoutDB',
+{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
